304ca3b3d450a2269a3b73767a8891a7c5d51802
nikita moraru <test@mail.com>
2020-04-29 00:15:31 +0300

    v1

---
+++ src/main/java/ru/netology/CommentsInfo.java
@@ -0,0 +1,14 @@
+package ru.netology;
+
+public class CommentsInfo {
+    private int commentId;
+    private int authorCommentId;
+    private String dateComment;
+    private String timeComment;
+    private String contentComment;
+    private int amountLikeComment;
+    private boolean canPost;
+
+    // + getters/setters
+
+}
---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,21 @@
+package ru.netology;
+
+public class Post {
+    private int postId;
+    private int authorId;
+    private String datePost;
+    private String timePost;
+    private String contentPost;
+    private int amountLike;
+    private int amountView;
+    private CommentsInfo commentsInfo;
+
+    // + getters/setters
+
+    public void isSaveToBookmarks() {
+    }
+
+    public void isShare() {
+    }
+    
+}
